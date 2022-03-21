0acdbda6fdf7f801cb536c9987bb5ca65fc1b32b
init.defaultBranch <benlimpic@gmail.com>
2022-03-21 09:02:21 -0700

    commit tree 7a3496b7e4816aa66d25ab4c1ca56370452d67f5

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Ben Limpic is the champion";
+document.body.append(newHeader);
